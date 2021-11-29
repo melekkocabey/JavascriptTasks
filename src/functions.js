4f5543ec4bc0ccad60225d0df43c0fc89d0b69d9
Melek <melekkocabey14@gmail.com>
2021-11-29 23:27:47 +0300

    commentler
    Signed-off-by: Melek <melekkocabey14@gmail.com>

--- src/functions.js
+++ src/functions.js
@@ -13,7 +13,7 @@ let sayHello = ()=>{
 
 sayHello()
 
-let sayHello2 = function () {
+ let sayHello2 = function () {
     console.log("Hello World 2")
 }
 
@@ -40,7 +40,7 @@ addToCart3(product1)
 
 let product2 = {productName:"Elma", unitPrice:10, quantity:5}
 let product3 = {productName:"Elma", unitPrice:10, quantity:5}
-product2 = product3
+product2 = product3 /* adrese değer atıyorsun örneğin product2 nin adresi 101 ise product3 ün adresi 102 ise atama sonucu 101 adresi artık 102 adresini tutuor demektir.102.adresinde ki product a karpuz at ve 102 de ki değeri yazdır dememktirf. */
 product2.productName = "KARPUZ"
 console.log(product3.productName)
 
@@ -49,6 +49,7 @@ let sayi2 = 20
 sayi1 = sayi2
 sayi2 = 100
 console.log(sayi1)
+console.log(sayi2)
 
 function addToCart4(products) {
     console.log(products)
@@ -60,24 +61,25 @@ let products = [
     {productName:"Karpuz", unitPrice:10, quantity:5}
 ]
 
-addToCart4(products)
+addToCart4(products[0].productName + " " + products[0].unitPrice)
 
-function add(bisey,...numbers) { //rest
+function add(bisey,...numbers) { //rest oparetorü >>> ... gönderilen parametreleri bir array içine koy, her zaman fonksiyonun sonuna bırakılmalı
     let total = 0;
     for (let i = 0; i < numbers.length; i++) {
         total = total + numbers[i]
+        console.log(numbers[i])
     }
     console.log(total)
     console.log(bisey)
 }
 
-add(20,30)
+add(20,30,40) //let numbers = [20,30] 20,30 array yap numbers a gönder
 //add(20,30, 40)
 //add(20,30, 40, 50)
 
 let numbers = [30,10,500,600,120]
 //console.log(...numbers)
-console.log(Math.max(...numbers))
+console.log(Math.max(...numbers)) //gönderilen sayılardan en büyüğünü bul
 
 let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
     {name:"İç Anadolu", population:"20M"},
@@ -89,15 +91,19 @@ let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
         ["Sinop","Trabzon"],
     ]
 ]
-
-// console.log(icAnadolu.name)
+let [icAnadolu1,marmara1] = ["İç Anadolu","Marmara"]
+ console.log(icAnadolu.name)
 // console.log(icAnadolu.population)
 console.log(icAnadoluSehirleri)
 
+//objeninde elemanlarını değişkene atama işlemi ,parçalama işlemi,verileri işlerken yararlı
 let newProductName, newUnitPrice, newQuantity
 ({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity } 
-= {productName:"Elma", unitPrice:10, quantity:5})
+= {productName:"Elma", unitPrice:10, quantity:5})//parantez içine alınmazsa süslü parantezleri blok olarak görür
 
 console.log(newProductName)
 console.log(newUnitPrice)
-console.log(newQuantity)
+console.log(newQuantity) 
+/*login({title}){
+    console.log("<h1>"+title+"</h1>");
+}login diyew bir ekrarn var ve bu şekilde çağrıldığında params gönder birden fazla params olduğunda böyle yazılır
